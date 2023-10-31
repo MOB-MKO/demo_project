775fd71a9e7e01fd2c098e5a9b0210a5b1453b2f
MOB-MKO <mukaramoladega@gmail.com>
2023-10-31 23:00:04 +0100

    'testing'

---
+++ www/app/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+  <p>Copyright (c) 2023 Copyright Holder All Rights Reserved.</p>
+</div>
---
+++ www/app/includes/header.php
@@ -0,0 +1,5 @@
+<div class="header">
+  <a href="index.php">HOME</a>
+  <a href="aboutus.php">ABOUT US</a>
+  <a href="contactus.php">CONTACT US</a>
+</div>
